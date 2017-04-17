b2922f26c6dd0ffe5b460e057515228b3c4ee0ee
Unknown <joseprojascapell@enti.cat>
2017-04-17 20:19:41 +0200

    Implementació Canvi de Nivell amb Trigger de Colliders i Art Bckgrnd

--- The Extraterrestial Spy/Assets/Scripts/Guardia Scripts/TriggerEvent.cs
+++ The Extraterrestial Spy/Assets/Scripts/Guardia Scripts/TriggerEvent.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class TriggerEvent : MonoBehaviour
 {
 
-
+   
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log(" EXTRATERRESTIAL DETECTED");
+            SceneManager.LoadScene("1", LoadSceneMode.Additive);
             
         }
     }
