55fe6c7a3d16e2a94e5f505b4d7625a8e5e517b4
Unknown <joseprojascapell@enti.cat>
2017-05-09 22:09:46 +0200

    Inici Nivell 2

--- The Extraterrestial Spy/Assets/Scripts/Guardia Scripts/CollisionEvent.cs
+++ The Extraterrestial Spy/Assets/Scripts/Guardia Scripts/CollisionEvent.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement; //Libreria per gestionar les escenes
 
 public class CollisionEvent : MonoBehaviour {
 
+    public int SceneCharger;
 
 
     public void OnTriggerEnter2D(Collider2D other) //Quan es detecta una entrada contra un trigger 2D per part de l'objecte al que se li assigna el collider:
@@ -12,7 +13,7 @@ public class CollisionEvent : MonoBehaviour {
         if (other.CompareTag("Player")) // Si el tag que entra dins el trigger es "PLAYER":
         {
             Debug.Log(" EXTRATERRESTIAL KILLED");
-            SceneManager.LoadScene(0);  //Carreguem el nivell de nou 
+            SceneManager.LoadScene(SceneCharger);  //Carreguem el nivell de nou 
 
         }
     }
