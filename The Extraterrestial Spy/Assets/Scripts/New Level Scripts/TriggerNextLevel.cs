bdda94f17ae67df41183b08a34f6c56d0f05ea68
Unknown <joseprojascapell@enti.cat>
2017-05-28 23:14:23 +0200

    Inici "nivell" final /menu/sons

--- The Extraterrestial Spy/Assets/Scripts/New Level Scripts/TriggerNextLevel.cs
+++ The Extraterrestial Spy/Assets/Scripts/New Level Scripts/TriggerNextLevel.cs
@@ -5,13 +5,14 @@ using UnityEngine.SceneManagement; //Libreria per gestionar les escenes
 
 public class TriggerNextLevel : MonoBehaviour
 {
+    public int nextscene;
 
     public void OnTriggerEnter2D(Collider2D other) //Quan el gameobject que contingui aquest script detecti ->
     {
         if (other.CompareTag("Player")) //-> que un altre gameobject amb el tag "PLAYER" entri dins el seu trigger,
         {
             
-            SceneManager.LoadScene(1);  // Carregarem el seguent nivell enumerat 1 dins l'ordre de build de nivells.
+            SceneManager.LoadScene(nextscene);  // Carregarem el seguent nivell enumerat 1 dins l'ordre de build de nivells.
 
         }
     }
